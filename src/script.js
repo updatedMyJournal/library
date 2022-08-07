5b5d5ee8c51609af2e2ca4f04c7af3da51598768
updatedMyJournal <99217422+updatedMyJournal@users.noreply.github.com>
2022-08-07 20:09:52 +0300

    Add Book and BookStorage classes, add book and storage utility functions

--- src/script.js
+++ src/script.js
@@ -1,1 +1,95 @@
 'use strict';
+
+let addButton = document.querySelector('.add');
+
+class BookStorage extends Map {
+  saveBook(bookObj) {
+    this.set(bookObj.index, bookObj);
+    this.#saveToLocalStorage();
+  }
+
+  deleteBook(bookObj) {
+    this.delete(bookObj.index);
+    this.#saveToLocalStorage();
+  }
+
+  #saveToLocalStorage() {
+    localStorage.setItem('bookStorage', JSON.stringify(this));
+  }
+
+  toJSON() {
+    return {
+      class: "BookStorage",
+      value: [...this]
+    }
+  }
+}
+
+class Book {
+  constructor({author, title, pages, read, index} = {}) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+    this.index = index;
+  }
+
+  toJSON() {
+    return {
+      class: "Book",
+      value: {...this}
+    }
+  }
+}
+
+let bookStorage = getFromLocalStorage() ?? new BookStorage();
+
+function displayBookCards() {
+  for (let book of bookStorage.values()) {
+    createBookCard(book);
+  }
+}
+
+function createBookCard({author, title, pages, read, index}) {
+  addButton.insertAdjacentHTML('beforebegin', 
+    `<div class="card" data-index=${index}>
+      <div class="author">${author}</div>
+      <div class="title">${title}</div>
+      <div class="pages">${pages} page(s)</div>
+      <button class="read${read ? " positive-read" : ""}">Read</button>
+      <div class="buttons-wrapper">
+        <button class="edit">Edit</button>
+        <button class="delete">Delete</button>
+      </div>
+     </div>`
+  );
+}
+
+function getFromLocalStorage() {
+  let str = localStorage.getItem('bookStorage');
+
+  if (!str) return;
+
+  let reviver = (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      if (value.class == 'BookStorage') {
+        return new BookStorage(value.value);
+      } else if (value.class == 'Book') {
+        return new Book(value.value);
+      }
+    }  
+
+      return value;
+  }
+
+  let obj = JSON.parse(str, reviver);
+
+  return obj;
+}
+
+function getBookObjFromBookStorage(elem) {
+  let card = getCard(elem)
+  let index = Number(card.dataset.index);
+
+  return bookStorage.get(index);
+}
